dcf24053ea72205378870d258156b83527ff8122
webwenhui1810 <45392098+webwenhui1810@users.noreply.github.com>
2019-01-25 20:03:43 +0800

    Add files via upload

---
+++ ms/app.js
@@ -0,0 +1,18 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const userRouter=require('./routes/user.js');
+
+//创建web服务器
+var server=express();
+server.listen(3000);
+//托管静态资源到public目录下
+server.use(express.static('public'));
+//使用body-parser中间件将post请求数据解析为对象
+//注意：一定要写在路由的前边
+server.use(bodyParser.urlencoded({
+  extended:false
+}));
+//把用户路由器挂载到/user
+// /user/register
+server.use('/user',userRouter);
+
